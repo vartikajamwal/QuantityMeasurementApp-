ced68b1a33706a129e50143d29005b50c0537ad6
vartikajamwal <vj2081@srmist.edu.in>
2026-04-16 15:09:51 +0530

    UC1: Feet measurement equality

---
+++ src/QuantityMeasurementApp.java
@@ -0,0 +1,25 @@
+public class QuantityMeasurementApp {
+
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet feet = (Feet) obj;
+            return Double.compare(this.value, feet.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println(f1.equals(f2));
+    }
+}
---
+++ test/QuantityMeasurementAppTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentClass() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(f1.equals("1.0"));
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f1));
+    }
+}
